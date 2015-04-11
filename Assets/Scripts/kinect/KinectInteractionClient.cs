23fe5c55d850666b742ef1b51d2745ada945f3d5
Alexandre Guyon <alex.guyon78@gmail.com>
2015-04-11 15:22:14 +0200

    Added interaction

---
+++ Assets/Scripts/kinect/KinectInteractionClient.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class KinectInteractionClient
+{
+    public enum InteractionHandEventType
+    {
+        None = 0,
+        Grip,
+        GripRelease
+    }
+
+    private TcpClient client;
+
+    private String Host = "127.0.0.1";
+    private Int32 Port = 8888;
+
+    private Thread thread;
+    private NetworkStream stream;
+
+    private InteractionHandEventType lastHandEventType = InteractionHandEventType.None;
+    private InteractionHandEventType currhandEventType = InteractionHandEventType.None;
+
+    public Vector2 handPosition = new Vector2(0, 0);
+
+    public void Run(int handType)
+    {
+        try
+        {
+            // Initialize client and its stream
+            client = new TcpClient(Host, Port);
+            stream = client.GetStream();
+
+            // Send to server the hand type
+            stream.WriteByte((Byte)handType);
+
+            // Start thread
+            thread = new Thread(new ThreadStart(HandleClientCommunication));
+            thread.Start();
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Error : " + e);
+            this.Close();
+        }
+    }
+
+    private void HandleClientCommunication()
+    {
+        while (true)
+        {
+            BinaryReader br = null;
+            try
+            {
+                br = new BinaryReader(stream);
+                
+                // Receive informations in the order that they've been sent
+                currhandEventType = (InteractionHandEventType)br.ReadInt32();
+                handPosition.x = (float)br.ReadDouble();
+                handPosition.y = (float)br.ReadDouble();
+                
+                // Don't take the interaction event "None"
+                if (currhandEventType != InteractionHandEventType.None)
+                    lastHandEventType = currhandEventType;
+
+            }
+            catch (Exception e)
+            {
+                Debug.Log("Error : " + e);
+                this.Close();
+                break;
+            }
+
+            if (br == null)
+            {
+                // Client has disconnected
+                Debug.Log("Disconnected");
+                this.Close();
+                break;
+            }
+        }
+    }
+
+    public bool HandIsClose()
+    {
+        if (lastHandEventType == InteractionHandEventType.Grip)
+            return true;
+
+        return false;
+    }
+
+    public bool HandIsOpen()
+    {
+        if (lastHandEventType == InteractionHandEventType.GripRelease)
+            return true;
+
+        return false;
+    }
+
+    public void Close()
+    {
+        stream.Close();
+        client.Close();
+    }
+}
---
+++ Assets/Scripts/kinect/KinectInteractionListener.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KinectInteractionListener : MonoBehaviour {
+    public int handType = 0;
+    public GUIText debug;
+
+    public GameObject virtual_hand;
+
+    public float followSpeed = 5.0f;
+
+    private Plane XZPlane = new Plane(Vector3.up, Vector3.zero);
+    private KinectInteractionManager manager;
+
+    private Ray ray;
+
+    
+
+	// Use this for initialization
+	void Start () {
+        manager = new KinectInteractionManager();
+        manager.init(handType, Screen.width, Screen.height);
+        manager.listenBehaviour(KinectInteractionBehaviour.GripAndFollow);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        manager.updateListenedBehaviours();
+
+        BehaviourData gripAndFollowData = manager.getBehaviourDataOf(KinectInteractionBehaviour.GripAndFollow);
+
+        debug.text = "State : " + (GripAndFollowState)gripAndFollowData.state;
+        debug.text += "\nPosition :" + gripAndFollowData.currPosition;
+
+        virtual_hand.transform.localPosition = GetPositionOnXZPlane(gripAndFollowData.screenPosition);
+
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+
+        if (gripAndFollowData.state == (int)GripAndFollowState.FOLLOW)
+        {
+            float moveH = virtual_hand.transform.position.x - transform.position.x;
+            float moveV = virtual_hand.transform.position.z - transform.position.z; 
+
+            Vector3 movement = new Vector3(moveH, 0, moveV);
+            rigidbody.AddForce(movement * followSpeed);
+        }
+            
+
+        //debug.transform.position = new Vector3(gripAndFollowData.currPosition.x/2, 1f - gripAndFollowData.currPosition.y , 0) ;
+	}
+
+    void OnApplicationQuit()
+    {
+        manager.Close();
+    }
+
+    
+
+    private Vector3 GetPositionOnXZPlane(Vector2 position)
+    {
+        float distance;
+        Ray ray = Camera.allCameras[handType].ScreenPointToRay(position);
+        if (XZPlane.Raycast(ray, out distance))
+        {
+            Vector3 hitPoint = ray.GetPoint(distance);
+            //Just double check to ensure the y position is exactly zero
+            hitPoint.y = 0;
+            return hitPoint;
+        }
+        return Vector3.zero;
+    }
+}
